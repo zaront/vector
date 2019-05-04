5759d23dcb001808dda392f797e52371e49b3370
Zaron <zaron.thompson@gmail.com>
2019-05-04 09:38:52 -0700

    no exceptions on event cancellations

--- Vector/TaskExtention.cs
+++ Vector/TaskExtention.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Grpc.Core;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,8 +13,16 @@ namespace Vector
 			task.ContinueWith(i =>
 			{
 				if (i.IsFaulted && i.Exception != null)
+				{
+					//don't throw if the task was cancelled
+					var rpcCanceled = i.Exception.InnerException as RpcException;
+					if (rpcCanceled != null && rpcCanceled.Status.StatusCode == StatusCode.Cancelled)
+						return;
+
 					throw i.Exception;
-			});
+				}
+					
+			}, TaskContinuationOptions.NotOnCanceled);
 		}
 	}
 }
